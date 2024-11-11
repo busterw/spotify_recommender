efb29e811065ef6f7663d36808b35149e59ce780
busterw <busterw@hotmail.co.uk>
2024-11-11 22:24:46 +0000

    lower case

--- src/main.rs
+++ src/main.rs
@@ -33,12 +33,10 @@ async fn get_recommendations_handler(params: web::Query<QueryParams>) -> impl Re
     let client_id = "cd8bff96f8544158868aa53aa4a7bd72";
     let client_secret = "d879110eade840b58a0da18a1dadb453";
 
-    println!("I'm hit!");
-
     let token = get_access_token(client_id, client_secret).await.unwrap();
 
-    let mood = &params.mood;
-    let genre = &params.genre;
+    let mood = &params.mood.to_lowercase();
+    let genre = &params.genre.to_lowercase();
 
     let tracks = get_recommendations(&token, mood, genre).await.unwrap_or_default();
 
